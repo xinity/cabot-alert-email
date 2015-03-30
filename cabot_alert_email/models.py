3856aec909a12fc90b4d6a01348fd22932d73845
Rachid Zarouali <rachid.zarouali@synolia.com>
2015-03-30 15:19:42 +0200

    crapy add instance send email alert

--- cabot_alert_email/models.py
+++ cabot_alert_email/models.py
@@ -10,7 +10,7 @@ from cabot.cabotapp.alert import AlertPlugin
 import requests
 import logging
 
-email_template = """Service {{ service.name }} {{ scheme }}://{{ host }}{% url 'service' pk=service.id %} {% if service.overall_status != service.PASSING_STATUS %}alerting with status: {{ service.overall_status }}{% else %}is back to normal{% endif %}.
+email_service_template = """Service {{ service.name }} {{ scheme }}://{{ host }}{% url 'service' pk=service.id %} {% if service.overall_status != service.PASSING_STATUS %}alerting with status: {{ service.overall_status }}{% else %}is back to normal{% endif %}.
 {% if service.overall_status != service.PASSING_STATUS %}
 CHECKS FAILING:{% for check in service.all_failing_checks %}
   FAILING - {{ check.name }} - Type: {{ check.check_category }} - Importance: {{ check.get_importance_display }}{% endfor %}
@@ -21,11 +21,21 @@ Passing checks:{% for check in service.all_passing_checks %}
 {% endif %}
 """
 
+email_instance_template = """Instance {{ instance.name }} {{ scheme }}://{{ host }}{% url 'instance' pk=instance.id %} {% if instance.overall_status != instance.PASSING_STATUS %}alerting with status: {{ instance.overall_status }}{% else %}is back to normal{% endif %}.
+{% if instance.overall_status != instance.PASSING_STATUS %}
+CHECKS FAILING:{% for check in instance.all_failing_checks %}
+  FAILING - {{ check.name }} - Type: {{ check.check_category }} - Importance: {{ check.get_importance_display }}{% endfor %}
+{% if service.all_passing_checks %}
+Passing checks:{% for check in service.all_passing_checks %}
+  PASSING - {{ check.name }} - Type: {{ check.check_category }} - Importance: {{ check.get_importance_display }}{% endfor %}
+{% endif %}
+{% endif %}
+"""
 class EmailAlert(AlertPlugin):
     name = "Email"
     author = "Jonathan Balls"
 
-    def send_alert(self, service, users, duty_officers):
+    def send_service_alert(self, service, users, duty_officers):
         emails = [u.email for u in users if u.email]
         if not emails:
             return
@@ -41,7 +51,31 @@ class EmailAlert(AlertPlugin):
                 service.overall_status, service.name)
         else:
             subject = 'Service back to normal: %s' % (service.name,)
-        t = Template(email_template)
+        t = Template(email_service_template)
+        send_mail(
+            subject=subject,
+            message=t.render(c),
+            from_email='Cabot <%s>' % env.get('CABOT_FROM_EMAIL'),
+            recipient_list=emails,
+        )
+
+    def send_instance_alert(self, instance, users, duty_officers):
+        emails = [u.email for u in users if u.email]
+        if not emails:
+            return
+        c = Context({
+            'instance': instance,
+            'host': settings.WWW_HTTP_HOST,
+            'scheme': settings.WWW_SCHEME
+        })
+        if instance.overall_status != instance.PASSING_STATUS:
+            if instance.overall_status == instance.CRITICAL_STATUS:
+                emails += [u.email for u in users if u.email]
+            subject = '%s status for instance: %s' % (
+                instance.overall_status, instance.name)
+        else:
+            subject = 'Instance back to normal: %s' % (service.name,)
+        t = Template(email_instance_template)
         send_mail(
             subject=subject,
             message=t.render(c),
